c931f8f6b699df60ebc316c01229dc9c4f40dd74
Arshan-Akhtar <arshanakhtar9100@gmail.com>
2025-09-05 17:02:59 +0530

    modified

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,16 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  base: '/',
 })
